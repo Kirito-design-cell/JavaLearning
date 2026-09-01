4ec9eebd5895a1ac9fe247f1b7270f41ded9bb5d
周建伟 <3565272336@qq.com>
2026-09-01 22:25:21 +0800

    refactor in Grade.java

--- src/Grade.java
+++ src/Grade.java
@@ -1,20 +1,18 @@
 public class Grade {
-    public static void main(String[] args){
-        int[] scores = {85,92,76,60,58};
-        for(int i =0;i<scores.length;i++) {
-            if (scores[i] >= 90) {
-                System.out.println(scores[i] + " -> A");
-            } else if (scores[i] >= 80) {
-                System.out.println(scores[i] + " -> B");
-            } else if(scores[i] >=70){
-                System.out.println(scores[i] + " -> C");
-            }else if(scores[i] >=60){
-                System.out.println(scores[i] + " -> D");
-            }else{
-                System.out.println(scores[i] + " -> E");
+    public static void main(String[] args) {
+        int[] scores = {85, 92, 76, 60, 58};
+        for (int score : scores) {
+            if (score >= 90) {
+                System.out.println(score + " -> A");
+            } else if (score >= 80) {
+                System.out.println(score + " -> B");
+            } else if (score >= 70) {
+                System.out.println(score + " -> C");
+            } else if (score >= 60) {
+                System.out.println(score + " -> D");
+            } else {
+                System.out.println(score + " -> E");
             }
         }
-
     }
-
 }
