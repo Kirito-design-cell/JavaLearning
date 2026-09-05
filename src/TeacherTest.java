058e2fdda7dba09f06935685dac9690031c805fe
周建伟 <3565272336@qq.com>
2026-09-06 00:10:38 +0800

    update TeacherTest subject

--- src/TeacherTest.java
+++ src/TeacherTest.java
@@ -1,7 +1,7 @@
 public class TeacherTest {
     public static void main(String[] args){
         Teacher t = new Teacher("Java", "王老师",40);
-        t.setSubject("Spring");
+        t.setSubject("math");
         t.introduce();
         System.out.println(t.getSubject());
     }
